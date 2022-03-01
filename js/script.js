4a30aba81caa35a31c638607b76a05b01b7687f1
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-01 17:28:19 +0100

    -esercizio due- devo ancora stilizzarlo

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@
 let word = prompt('Inserisci una parola e verifica se questa è palindroma o meno');
 let output = document.querySelector('.output');
 
-
 let reverseWord = checkPalindrome(word);
 
 if(word.toLowerCase() == reverseWord.toLowerCase()){
@@ -20,6 +19,56 @@ function checkPalindrome(string){
   let stringReverse = string.split('').reverse().join('');  
   return stringReverse;
 }
+
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+let question = prompt('Scegli: Pari o Dispari?').toLowerCase();
+let num = parseInt(prompt('Inserisci un numero da 1 a 5'));
+let outputTwo = document.querySelector('.output-two');
+let outputThree = document.querySelector('.output-three');
+let outputFour = document.querySelector('.output-four');
+let outputFive = document.querySelector('.output-five');
+let numCpu = random(5);
+
+outputTwo.innerHTML = `Il tuo numero: ${num}`;
+outputThree.innerHTML = `Numero Cpu: ${numCpu}`; 
+outputFour.innerHTML = `Totale: ${sum()}`;
+
+if (question == 'pari' && sum() % 2 == 0) {
+    outputFive.innerHTML = 'Hai vinto!!';
+} else if (question == 'dispari' && sum() % 2 == 0) {
+    outputFive.innerHTML = 'Hai perso!!';
+} else if (question == 'dispari' && sum() % 2 ==1) {
+    outputFive.innerHTML = 'Hai vinto!!';
+} else {
+    outputFive.innerHTML = 'Hai perso!!';
+}
+
+function random(max) {
+    return Math.floor(Math.random() * (max) ) + 1;
+}
+
+function sum() {
+    return num + numCpu;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
